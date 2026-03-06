9cd525c75969bd15eb70851357b733d6e40b0993
Daniel Rákos (RasterGrid) <daniel.rakos@rastergrid.com>
2026-03-06 15:27:52 +0100

    tests: Fix TestOnlyDriverEnvVarInFolder workaround precondition

--- tests/loader_envvar_tests.cpp
+++ tests/loader_envvar_tests.cpp
@@ -159,12 +159,13 @@ TEST(EnvVarICDOverrideSetup, TestOnlyDriverEnvVarInFolder) {
     EXPECT_FALSE(
         env.debug_log.find("Ignoring override VK_ICD_FILENAMES, VK_DRIVER_FILES, and VK_ADD_DRIVER_FILES due to high-integrity"));
 
-    // The availability of Get Device Properties 2 and using linux device sort cause duplicately reported devices
+#if defined(VULKANSC)  // The availability of GPDP2 in Vulkan SC and using linux device sort cause duplicately reported devices
 #if TESTING_COMMON_UNIX_PLATFORMS
     std::array<VkPhysicalDevice, 9> phys_devs_array;
 #else
     std::array<VkPhysicalDevice, 5> phys_devs_array;
 #endif
+#endif  // VULKANSC
     uint32_t phys_dev_count = 1;
     ASSERT_EQ(inst1->vkEnumeratePhysicalDevices(inst1.inst, &phys_dev_count, phys_devs_array.data()), VK_SUCCESS);
     ASSERT_EQ(phys_dev_count, 1U);
@@ -178,7 +179,7 @@ TEST(EnvVarICDOverrideSetup, TestOnlyDriverEnvVarInFolder) {
     InstWrapper inst2{env.vulkan_functions};
     inst2.CheckCreate();
 
-    // The availability of Get Device Properties 2 and using linux device sort cause duplicately reported devices
+#if defined(VULKANSC)  // The availability of GPDP2 in Vulkan SC and using linux device sort cause duplicately reported devices
 #if TESTING_COMMON_UNIX_PLATFORMS
     phys_dev_count = 9;
     ASSERT_EQ(inst2->vkEnumeratePhysicalDevices(inst2.inst, &phys_dev_count, phys_devs_array.data()), VK_SUCCESS);
@@ -188,6 +189,7 @@ TEST(EnvVarICDOverrideSetup, TestOnlyDriverEnvVarInFolder) {
     ASSERT_EQ(inst2->vkEnumeratePhysicalDevices(inst2.inst, &phys_dev_count, phys_devs_array.data()), VK_SUCCESS);
     ASSERT_EQ(phys_dev_count, 5U);
 #endif
+#endif  // VULKANSC
 }
 // Test VK_DRIVER_FILES environment variable containing a path to a folder  with elevated privileges
 TEST(EnvVarICDOverrideSetup, TestOnlyDriverEnvVarInFolderWithElevatedPrivileges) {
